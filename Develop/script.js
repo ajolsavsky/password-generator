b7b6e2c6111bfd47c450c8678efb9d2161b038d6
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-04-20 16:39:52 -0500

    Updated to build password by using array, still within one function

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,9 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var characters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+// var characters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+//Create empty string for character confirmations to push into if validated
+var characterArray = [];
 
 //Password Generator Function
 function generatePassword() {
@@ -44,6 +46,9 @@ var uppercase = window.confirm("Should we include uppercase letters?")
 
 if(uppercase){
   alert("YES - we will include uppercase letters.")
+  // characterArray[0] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  characterArray[0] = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+
 }
 
 if(!uppercase){
@@ -58,6 +63,7 @@ var lowercase = window.confirm("Should we include lowercase letters?")
 
 if(lowercase){
   alert("YES - we will include lowercase letters.")
+  characterArray[1] = ["abcdefghijklmnopqrstuvwxyz"]
 }
 
 if(!lowercase){
@@ -71,7 +77,8 @@ console.log("the value of lowercase is " + lowercase);
 var numeric = window.confirm("Should we include numeric values?")
 
 if(numeric){
-  alert("YES - we will include numeric values.")
+  alert("YES - we will include numeric values.");
+  characterArray[2] = ["0123456789"]
 }
 
 if(!numeric){
@@ -85,7 +92,8 @@ console.log("the value of numeric is " + numeric);
 var specialCharacters = window.confirm("Should we include special characters?")
 
 if(specialCharacters){
-  alert("YES - we will include special characters.")
+  alert("YES - we will include special characters.");
+  characterArray[3] = ["!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
 }
 
 if(!specialCharacters){
@@ -95,16 +103,24 @@ if(!specialCharacters){
 
 console.log("the value of specialCharacters is " + specialCharacters)
 
+// Final log of all user choices
 console.log("Uppercase = " + uppercase + "\nLowercase = " + lowercase + "\nNumeric = " + numeric + "\nSpecial = " + specialCharacters)
 
+//Create alert if all four cases were negative, return to top of function
 if(uppercase === false && lowercase === false && numeric === false && specialCharacters === false){
   alert('Error: No characters were selected. Please click "OK" for at least 1 of the following: Uppercase, Lowercase, Numeric, Special Characters')
   generatePassword();
 }
 
+//Convert existing array into single string of characters with no spaces
+var characterArrayText = characterArray.join("");
+console.log(characterArrayText);
+
+//Function to call to make an id at random from the string (converted from array)
 function makeid(length) {
     var result = '';
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    // var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    var characters = characterArrayText;
     var charactersLength = characters.length;
     for (var i=0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -113,6 +129,13 @@ function makeid(length) {
 
   }
 
-  alert("Your new password is: " + makeid(userCharLength));
+//Creates window alert by calling makeid function at the length the user initially entered
+window.alert("Your new password is: " + makeid(userCharLength));
+
+
+var password = makeid(userCharLength);
+console.log(password);
+
+writePassword;
 
 }
