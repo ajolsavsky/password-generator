4116a3ee58aca8b0b6b664359d74e66dd13366b3
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-04-20 18:29:15 -0500

    Created new set of functions and added return values - password now writes to screen

--- Develop/script.js
+++ Develop/script.js
@@ -13,11 +13,13 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// var characters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-//Create empty string for character confirmations to push into if validated
+////
+
+// Create empty string for character confirmations to push into if validated
 var characterArray = [];
 
-//Password Generator Function
+
+// Password Generator Function
 function generatePassword() {
 
   // Character Length Prompt
@@ -42,100 +44,112 @@ function generatePassword() {
   }
 
 // Uppercase validation
-var uppercase = window.confirm("Should we include uppercase letters?")
-
-if(uppercase){
-  alert("YES - we will include uppercase letters.")
-  // characterArray[0] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  characterArray[0] = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-
-}
-
-if(!uppercase){
-  alert("NO - we will not include uppercase letters.")
-  uppercase = false;
-}
-
-console.log("the value of uppercase is " + uppercase);
+var uppercase = confirmUppercase();
 
 // Lowercase validation
-var lowercase = window.confirm("Should we include lowercase letters?")
-
-if(lowercase){
-  alert("YES - we will include lowercase letters.")
-  characterArray[1] = ["abcdefghijklmnopqrstuvwxyz"]
-}
-
-if(!lowercase){
-  alert("NO - we will not include lowercase letters.")
-  lowercase = false;
-}
-
-console.log("the value of lowercase is " + lowercase);
+var lowercase = confirmLowercase();
 
 // Numeric validation
-var numeric = window.confirm("Should we include numeric values?")
-
-if(numeric){
-  alert("YES - we will include numeric values.");
-  characterArray[2] = ["0123456789"]
-}
-
-if(!numeric){
-  alert("NO - we will not include numeric values.")
-  numeric = false;
-}
-
-console.log("the value of numeric is " + numeric);
+var numeric = confirmNumeric();
 
 // Special characters
-var specialCharacters = window.confirm("Should we include special characters?")
+var specialCharacters = confirmSpecialCharacters();
 
-if(specialCharacters){
-  alert("YES - we will include special characters.");
-  characterArray[3] = ["!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
-}
-
-if(!specialCharacters){
-  alert("NO - we will not include special characters.")
-  specialCharacters = false;
-}
-
-console.log("the value of specialCharacters is " + specialCharacters)
-
-// Final log of all user choices
-console.log("Uppercase = " + uppercase + "\nLowercase = " + lowercase + "\nNumeric = " + numeric + "\nSpecial = " + specialCharacters)
-
-//Create alert if all four cases were negative, return to top of function
+// Create alert if all four cases were negative, return to top of function
 if(uppercase === false && lowercase === false && numeric === false && specialCharacters === false){
   alert('Error: No characters were selected. Please click "OK" for at least 1 of the following: Uppercase, Lowercase, Numeric, Special Characters')
-  generatePassword();
+  characterValidationSeries();
 }
 
-//Convert existing array into single string of characters with no spaces
+// Convert existing array into single string of characters with no spaces
 var characterArrayText = characterArray.join("");
-console.log(characterArrayText);
 
 //Function to call to make an id at random from the string (converted from array)
-function makeid(length) {
-    var result = '';
-    // var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-    var characters = characterArrayText;
-    var charactersLength = characters.length;
-    for (var i=0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+  var result = '';
+  var characters = characterArrayText;
+  var charactersLength = characters.length;
+  for (var i=0; i < userCharLength; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
+// Uppercase function
+function confirmUppercase() {
+  var uppercase = window.confirm("Should we include uppercase letters?")
+
+  if(uppercase){
+    alert("YES - we will include uppercase letters.")
+    characterArray[0] = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  }
+  
+  else{
+    alert("NO - we will not include uppercase letters.")
+    uppercase = false;
+  }
+  
+  console.log("the value of uppercase is " + uppercase);
+  return uppercase;
+};
+
+// Lowercase function
+function confirmLowercase() {
+  var lowercase = window.confirm("Should we include lowercase letters?")
+
+  if(lowercase){
+    alert("YES - we will include lowercase letters.")
+    characterArray[1] = ["abcdefghijklmnopqrstuvwxyz"]
   }
 
-//Creates window alert by calling makeid function at the length the user initially entered
-window.alert("Your new password is: " + makeid(userCharLength));
+  else{
+    alert("NO - we will not include lowercase letters.")
+    lowercase = false;
+  }
 
+  return lowercase;
 
-var password = makeid(userCharLength);
-console.log(password);
+};
 
-writePassword;
+// Numeric function
+function confirmNumeric() {
+  var numeric = window.confirm("Should we include numeric values?")
 
+  if(numeric){
+    alert("YES - we will include numeric values.");
+    characterArray[2] = ["0123456789"]
+  }
+
+  else{
+    alert("NO - we will not include numeric values.")
+    numeric = false;
+  }
+
+  return numeric;
+};
+
+// Special characters function
+function confirmSpecialCharacters() {
+  var specialCharacters = window.confirm("Should we include special characters?")
+
+  if(specialCharacters){
+    alert("YES - we will include special characters.");
+    characterArray[3] = ["!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+  }
+
+  else{
+    alert("NO - we will not include special characters.")
+    specialCharacters = false;
+  }
+
+  return specialCharacters;
+};
+
+function characterValidationSeries() {
+  var uppercase = confirmUppercase();
+  var lowercase = confirmLowercase();
+  var numeric = confirmNumeric();
+  var specialCharacters = confirmSpecialCharacters();
+
+  return uppercase, lowercase, numeric, specialCharacters;
 }
+
